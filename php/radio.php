da5de16213f1c91620d4154b4ab0a0475adf2aa8
Agent77326 <agent77326@gmail.com>
2017-09-02 17:37:20 +0200

    Remove depricated data

--- php/radio.php
+++ php/radio.php
@@ -1,16 +1,10 @@
 <?php
-	/*
-	require '/var/www/html/php/sinusbot.class.php';
-	$sinusbot = new SinusBot("http://agent77326.tk:8087");
-	$sinusbot->login("admin", "5207aq00rPvh");
-	echo $sinusbot->getWebStream($sinusbot->getInstances()[0]['uuid']);
-	*/
 	function scanDire($ordner){
 		$dir = array_diff(scandir($ordner), array('..', '.', 'index.php'));
 		sort($dir);
 		return $dir;
 	}
-	
+
 	switch($_GET['playlist']){
 		case "nc":
 			$scan = scanDire("/media/drive_2/www/media/audio/NC/");
